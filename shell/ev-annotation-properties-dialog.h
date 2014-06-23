d6577cc6184162d899680796a30d182f141dd5f6
Anuj Khare <khareanuj18@gmail.com>
2014-06-23 20:15:29 +0530

    shell: Add font property of freetext annotations

--- shell/ev-annotation-properties-dialog.h
+++ shell/ev-annotation-properties-dialog.h
@@ -48,6 +48,7 @@ void                 ev_annotation_properties_dialog_get_rgba            (EvAnno
 gdouble              ev_annotation_properties_dialog_get_opacity         (EvAnnotationPropertiesDialog *dialog);
 gboolean             ev_annotation_properties_dialog_get_popup_is_open   (EvAnnotationPropertiesDialog *dialog);
 EvAnnotationTextIcon ev_annotation_properties_dialog_get_text_icon       (EvAnnotationPropertiesDialog *dialog);
+const gchar         *ev_annotation_properties_dialog_get_font            (EvAnnotationPropertiesDialog *dialog);
 
 G_END_DECLS
 
